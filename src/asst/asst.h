ba849e5399a10d0d2deecbbd62e3685aa2c2c2e9
ahmad <aza29@mail.aub.edu>
2024-10-15 19:30:49 +0300

    Added headers and changed structs.

--- src/asst/asst.h
+++ src/asst/asst.h
@@ -5,24 +5,28 @@
 #ifndef ASST_H
 #define ASST_H
 #define GRID_SIZE 10
+#define NUM_SHIPS 4
+#define min(a, b) ((a) < (b) ? (a) : (b))
 
 typedef struct
 {
-    int ship1;
-    int ship2;
-    int ship3;
-    int ship4;
-} Ships;
-
-typedef struct
-{
-    Ships ships;
+    int* ships;
     int radar_sweep;
     int smoke_screen;
     int artillery;
     int torpedo;
     int** grid;
 } Player;
+
+void print_grid(int** grid);
+
+int update_torpedo(Player* attacker, Player* defender, int is_sunk);
+
+int is_sunk(Player* p, int ship_number);
+
+int fire(Player* p, int x, int y);
+
+int artillery(Player* attacker, Player* defender, int x, int y);
 
 int** initialize_grid();
 
